45a4dead675c11e77496d62e575265468f67c428
mov <mov51@mov51.net>
2023-02-11 18:34:23 -0500

    Actual Initial Commit

---
+++ src/main/java/net/mov51/minecraftauthroles/MinecraftAuthRoles.java
@@ -0,0 +1,18 @@
+package net.mov51.minecraftauthroles;
+
+import me.minecraftauth.lib.AuthService;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftAuthRoles extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
