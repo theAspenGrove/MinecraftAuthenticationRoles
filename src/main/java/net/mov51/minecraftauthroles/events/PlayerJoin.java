478fd147ff88e1e379f41ac4cd79fe2baf6d747b
mov <mov51@mov51.net>
2023-02-12 18:39:21 -0500

    Async me baby

--- src/main/java/net/mov51/minecraftauthroles/events/PlayerJoin.java
+++ src/main/java/net/mov51/minecraftauthroles/events/PlayerJoin.java
@@ -2,16 +2,26 @@ package net.mov51.minecraftauthroles.events;
 
 import net.mov51.minecraftauthroles.util.ConfigHelper;
 import net.mov51.minecraftauthroles.util.RoleToSync;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import static net.mov51.minecraftauthroles.MinecraftAuthRoles.plugin;
 
 public class PlayerJoin implements Listener {
     @EventHandler
     public void PlayerJoin(PlayerJoinEvent e) {
-        //check every player that joins for their current role status
-        for (RoleToSync role : ConfigHelper.roles.values()) {
-            role.handleUser(e.getPlayer());
-        }
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+            @Override
+            public void run() {
+                //check every player that joins for their current role status
+                for (RoleToSync role : ConfigHelper.roles.values()) {
+                    role.handleUser(e.getPlayer());
+                }
+            }
+        });
+
     }
 }
